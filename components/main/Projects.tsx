086f748371debb20fb824202053e0fc11af1a892
Tosin Ogunjobi <ogunjobi.developer@gmail.com>
2024-02-27 10:22:21 -0800

    update

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,40 +10,40 @@ const Projects = () => {
       </h1>
       <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 px-10">
         <ProjectCard
-          src="/project-1.png"
+          src="/latern.png"
           title="Latern"
           description="Open Source financial planning web application for small businesses."
-          link="https://saint-stream-peach.vercel.app"
+          link="https://github.com/oogunjob/Lantern-Client"
         />
         <ProjectCard
-          src="/project-2.png"
+          src="/project-3.png"
           title="Melody Migrate"
           description="Open source web application to transfer music playlists between different streaming platforms."
-          link="https://dev-overflow-alimogh.vercel.app"
+          link="https://github.com/oogunjob/Melody-Migrate"
         />
         <ProjectCard
           src="/project-3.png"
           title="Transcript Translations"
           description="Feature added to Microsoft Stream that allows users to translate transcripts of audio into over 120 languages."
-          link="https://car-showcase-livid-mu.vercel.app"
+          link="https://go.microsoft.com/fwlink/?linkid=2247340"
         />
         <ProjectCard
           src="/project-4.png"
           title="FX Telegram Copy Trader"
           description="Open source tool that allows Forex traders to copy trades from Telegram to their own trading platform with auto parsing leveraged from GPT-4."
-          link="https://next-commerce-one-ivory.vercel.app"
+          link="https://github.com/ogunjobiFX/MT4-MT5-Forex-Signal-Copier-Telegram-Bot"
         />
         <ProjectCard
           src="/project-5.png"
           title="STM32F091 Fruit Ninja Clone"
           description="Senior project that allows users to play the popular game Fruit Ninja on a STM32F091 microcontroller written in C."
-          link="https://quantum-alimogh.vercel.app"
+          link="https://github.com/oogunjob/STM32F091-Fruit-Ninja-Clone"
         />
         <ProjectCard
           src="/project-6.png"
           title="Music Lineup IO"
           description="Upload your course, price it, and let people watch it."
-          link="https://insightq.vercel.app"
+          link="https://github.com/oogunjob/Music-Lineup-IO"
         />
       </div>
     </div>
